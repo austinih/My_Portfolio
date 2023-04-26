83bb0af68abbace8106c1b01dd1cd8ffdc73c212
Austin <austinih46@gmail.com>
2023-04-26 16:16:32 -0500

    initial portfolio

--- my-portfolio/src/components/Nav.jsx
+++ my-portfolio/src/components/Nav.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div className='navBar'>
-            <Link to="/" className='navLinks'>
+        <div className='text-green-500 flex center'>
+            <Link to="/" className=''>
                 <h2>Home</h2>
             </Link>
-            <Link to="/about" className='navLinks'>
+            <Link to="/about" className=''>
                 <h2>About Me</h2>
             </Link>
-            <Link to="/projects" className='navLinks'>
+            <Link to="/projects" className=''>
                 <h2>Projects</h2>
             </Link>
             <Link to="/Contact" className='navLinks'>
