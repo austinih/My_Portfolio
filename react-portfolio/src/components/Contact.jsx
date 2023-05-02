692183b15a0d1514ab930226806de11186be530e
Austin <austinih46@gmail.com>
2023-05-02 15:36:43 -0500

    Finished Main Draft. Ready for Deployment

--- react-portfolio/src/components/Contact.jsx
+++ react-portfolio/src/components/Contact.jsx
@@ -8,6 +8,7 @@ export default function Contact() {
             <div className='flex flex-col justify-start w-96 mx-5 md:mx-36'>
                 <h2 className=' text-left text-white text-3xl'>Contact Me</h2>
                 <p className='text-left text-white text-xl mt-4'>Feel free to reach out via your preferred method. I look forward to connecting with you!</p>
+                <p className='text-left text-white text-xl mt-4'>Austin Holland - Austin, TX</p>
                 <div className='flex text-left text-white mt-4'>
                     <AiFillPhone className='text-left text-2xl mr-4 '/>
                     <p>+ 1 (702) 302 - 1772</p>
@@ -19,18 +20,18 @@ export default function Contact() {
             </div>
             <div className='mt-8 w-screen border-2 border-white mx-auto rounded-2xl md:w-0 md:h-56  md:mt-0 md:mx-0'></div>
             <ul className='mt-8 flex   items-center justify-center  text-gray-700 md:items-start md:align-top  md:mx-36 md:flex-col md:mt-0'>
-                <li className='cursor-pointer px-4 pb-2 text-7xl text-white'>
+                <li className='cursor-pointer px-4 pb-2 text-7xl text-white hover:text-gray-800 '>
                     <a href="https://www.linkedin.com/in/austinih/" target="_blank" >
                     <AiFillLinkedin/>
                     </a>
                 </li>
-                <li className='cursor-pointer px-4 pb-2 text-7xl text-white'>
+                <li className='cursor-pointer px-4 pb-2 text-7xl text-white hover:text-gray-800'>
                     <a href="https://github.com/austinih" target="_blank">
                     <AiFillGithub/>
                     </a> 
                 </li>
                 <li>
-                    <button onClick={() => window.open('mailto:austinih46@gmail.com', '_blank')} className='cursor-pointer px-4 pb-2 text-7xl text-white'>
+                    <button onClick={() => window.open('mailto:austinih46@gmail.com', '_blank')} className='cursor-pointer px-4 pb-2 text-7xl text-white hover:text-gray-800' >
                         <MdEmail />
                     </button>
                 </li>
